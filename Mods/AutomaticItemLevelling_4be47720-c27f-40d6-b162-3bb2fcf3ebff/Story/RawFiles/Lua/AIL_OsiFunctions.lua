2402c7c98c3d4ff9c3bf9eb2502af499a19fcf3d
frasdoge <frasdoge@gmail.com>
2020-06-01 02:40:41 +1000

    Rarity-based descaling.

--- Mods/AutomaticItemLevelling_4be47720-c27f-40d6-b162-3bb2fcf3ebff/Story/RawFiles/Lua/AIL_OsiFunctions.lua
+++ Mods/AutomaticItemLevelling_4be47720-c27f-40d6-b162-3bb2fcf3ebff/Story/RawFiles/Lua/AIL_OsiFunctions.lua
@@ -3,6 +3,7 @@ local function RarityUpOrDownScale(item)
     local rarity = NRD_ItemGetString(item, "ItemTypeReal")
     local rarityEnabled = next(Osi.DB_AIL_RarityActive:Get(rarity)) ~= nil
     
+    
     if (rarityEnabled) then
         -- Ext.Print("Rarity Active")
         return true
@@ -10,8 +11,10 @@ local function RarityUpOrDownScale(item)
 
     -- Ext.Print("Rarity Inactive")
 
+    local rarityDownscaling = GlobalGetFlag("AIL_RarityDownscale") == 1
     local itemLevelList = Osi.DB_AIL_ItemList:Get(item, nil)[1]
-    if (next(itemLevelList) ~= nil and GlobalGetFlag("AIL_RarityDownscale")) then
+
+    if ((next(itemLevelList) ~= nil) and (rarityDownscaling)) then
         local oLevel = itemLevelList[2]
         ItemLevelUpTo(item, oLevel)
     end
