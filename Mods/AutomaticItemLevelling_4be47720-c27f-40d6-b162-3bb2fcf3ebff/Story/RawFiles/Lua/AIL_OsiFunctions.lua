ea8e0e719400b33e72c9c956ace542347e20bfbd
frasdoge <frasdoge@gmail.com>
2020-06-30 14:46:28 +1000

    Bugfix to items being added more than once to DB, and console error from Lua

--- Mods/AutomaticItemLevelling_4be47720-c27f-40d6-b162-3bb2fcf3ebff/Story/RawFiles/Lua/AIL_OsiFunctions.lua
+++ Mods/AutomaticItemLevelling_4be47720-c27f-40d6-b162-3bb2fcf3ebff/Story/RawFiles/Lua/AIL_OsiFunctions.lua
@@ -1,25 +1,22 @@
-local function RarityUpOrDownScale(item)
+local function RarityUpOrDownScale(item, durability)
 
     local rarity = NRD_ItemGetString(item, "ItemTypeReal")
-    local rarityEnabled = next(Osi.DB_AIL_RarityActive:Get(rarity)) ~= nil
-    
+    local rarityEnabled = Osi.DB_AIL_RarityActive:Get(rarity)[1] ~= nil
     
     if (rarityEnabled) then
-        -- Ext.Print("Rarity Active")
         return true
     end
 
-    -- Ext.Print("Rarity Inactive")
-
     local rarityDownscaling = GlobalGetFlag("AIL_RarityDownscale") == 1
     local itemLevelList = Osi.DB_AIL_ItemList:Get(item, nil)[1]
+    local oLevel = itemLevelList[2]
 
-    if ((next(itemLevelList) ~= nil) and (rarityDownscaling)) then
-        local oLevel = itemLevelList[2]
+    if ((oLevel ~= nil) and (rarityDownscaling)) then
         ItemLevelUpTo(item, oLevel)
+        ItemSetDurability(item, durability)
     end
 
     return false
 
 end
-Ext.NewQuery(RarityUpOrDownScale, "AIL_RarityUpOrDownScale", "[in](ITEMGUID)_Item")
+Ext.NewQuery(RarityUpOrDownScale, "AIL_RarityUpOrDownScale", "[in](ITEMGUID)_Item, [in](INTEGER)_Durability")
